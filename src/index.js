298b1a6a7b4c895840cf844efcd5fec8dbb0086d
Michael Ko <mk872@cornell.edu>
2018-12-08 22:05:01 -0500

    Add question and answer populating functions, clickers determine right/wrong

--- src/index.js
+++ src/index.js
@@ -40,12 +40,11 @@ window.$l.ajax = (options) => {
       const xhr = new XMLHttpRequest();
       xhr.open(request.method, request.url);
       xhr.onload = () => {
-        if (xhr.status === 200) {
           resolve(JSON.parse(xhr.response));
-        } else {
-          reject(JSON.parse(xhr.response));
-        }
-      }
+      };
+      xhr.onerror = () => {
+          reject(JSON.parse(xhr.statusText));
+      };
       xhr.send(JSON.stringify(request.data));
     }
   )
@@ -54,4 +53,60 @@ window.$l.ajax = (options) => {
 document.addEventListener("DOMContentLoaded", function(){
   docLoaded = true;
   functionQueue.forEach(func => func());
+  $l('.start-btn').on('click', (e) => {
+    e.preventDefault();
+    $l('.start-btn').html("RESTART");
+    retrieveQuestions(3);
+  });
 });
+
+const retrieveQuestions = (n) => {
+  $l.ajax({
+    method: "GET",
+    url: `https://opentdb.com/api.php?amount=${n}`
+  }).then(response => displayQuestions(response.results));
+}
+
+const displayQuestions = (results) => {
+  const list = $l('.question-list');
+  list.empty();
+  results.forEach( (result, i) => {
+    list.append(`
+      <li id="q-${i}" class="question">
+        <h3>${result.question}</h3>
+        <ul class="ans-list ans-${i}">
+        </ul>
+      </li>`
+    );
+  });
+  results.forEach((result, i) => {
+    setupAnswers(result, i);
+  })
+  $l('.question-list').removeClass('hidden');
+
+}
+
+const setupAnswers = (result, i) => {
+  const answers = $l(`.ans-${i}`);
+  const result_ans = result.incorrect_answers.concat([result.correct_answer]);
+  result_ans.forEach((ans, j) => {
+    answers.append(`<li id='q-${i}-c-${j}' class='ans-choice'>${ans}</li>`);
+  })
+
+  result_ans.forEach((ans, j) => {
+    $l(`#q-${i}-c-${j}`).on('click', () => {
+      if (result.correct_answer === answers.children().nodes[j].innerHTML) {
+        $l(`#q-${i}`).removeClass('red');
+        $l(`#q-${i}`).addClass('green');
+        $l(`#q-${i}-c-${j}`).addClass('bold');
+        $l(`.ans-${i}`).children().off('click');
+      } else {
+        $l(`#q-${i}`).removeClass('green');
+        $l(`#q-${i}`).addClass('red');
+        $l(`#q-${i}-c-${j}`).addClass('bold');
+        $l(`.ans-${i}`).children().off('click');
+      }
+    })
+  })
+
+}
